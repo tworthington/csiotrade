77099dfde8388854302ae5f200b4fff1d894496b
Thomas Worthington <thomas@technouveau.co.uk>
2016-08-29 14:13:06 +0100

    Moved Emacs local vars into directory local file

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,25 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
+
+((forth-mode
+  (forth-local-words
+   (("times")
+    compile-only
+    (font-lock-keyword-face . 3))
+   (("iterate")
+    compile-only
+    (font-lock-keyword-face . 3))
+   (("-if")
+    compile-only
+    (font-lock-keyword-face . 3)))
+  (forth-local-indent-words
+   (("times")
+    (0 . 2)
+    (0 . 2))
+   (("iterate")
+    (-2 . 0)
+    (0 . -2))
+   (("-if")
+    (0 . 2)
+    (0 . 2)))))
+
